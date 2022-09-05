ff1d69e02ccd52910f2ede25d30d243589014a73
Atov <gognoqqq@gmail.com>
2022-09-05 13:32:39 +0300

    HW1

---
+++ task08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите число");
+int  N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N+1];
+
+for ( int i = 0; i<=N; i++)
+{
+    array[i] = i;
+    while (array[i] % 2 = 0)
+{
+    Console.WriteLine(array[i]);
+    i++;
+}
+
+}
